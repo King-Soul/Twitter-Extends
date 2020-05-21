5bfe73a638dd1f9f8c6923e8e841886abf980c67
Paulo <paulo.tavares@my.istec.pt>
2020-05-21 06:30:45 +0100

    Twitter Extends

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>EXTEND YOUR TWITTER HISTORY</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/style/main.css">
+    <link rel="stylesheet" type="text/css" href="/fontawesome/css/all.css">
+  </head>
+  <body>
+  <div class="container justify-content-center mt-2">
+  <div class="row">
+
+        <div class="col-lg-3" style="background-color:blue;">
+            <h3>
+                teste
+            </h3>
+        </div>
+
+        <div class="col-lg-6 main-content">
+            <img src="/img/back.jpg" class="img-fluid imgPerfil" alt="Responsive image">
+        </div>
+
+        <div class="col-lg-3" style="background-color:blue;">
+            <h3>
+                teste
+            </h3>
+        </div>
+
+    </div>
+
+    <div class="row">
+    <div class="col-lg-3" style="background-color:blue;">
+            <h3>
+                teste
+            </h3>
+        </div>
+
+        <div class="col-lg-6 main-content" >
+            <div class="row">
+                <div class="col-lg-4">
+                    <!-- Lado Esquerdo Do Perfil -->
+                </div>
+
+                <div class="col-lg-4">
+                    <img src="/img/avatar.png" class="img-fluid rounded-circle d-block mx-auto imgAvatar" alt="Responsive image"> 
+                </div>
+
+                <div class="col-lg-4 mt-2">    
+                    <div class="row justify-content-center">
+                        <button class="btnDetails"><i class="fas fa-ellipsis-h"></i></button>
+                        <button class="btnMessage"><i class="fas fa-envelope"></i></button>
+                        <button class="btnFollow">Follow</button>
+                    </div>              
+                </div>
+                
+            </div>
+            <div class="col-lg-12 text-center nomePerfil pt-2">
+                <p>KingSoul</p>
+            </div>
+
+            <div class="row profileDetails">
+                <div class="col-lg-4"></div>
+
+                <div class="col-lg-4 align-bottom">
+                    <div class="row">
+                        <div class="col-sm">
+                            <p class="arroba">@KingSoul</p>
+                        </div><div class="col-sm">
+                            <p class="arrobaFollow">@KingSoul</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4"></div>
+            </div>
+
+        </div>
+
+        <div class="col-lg-3" style="background-color:blue;">
+            <h3>
+                teste
+            </h3>
+        </div>
+    
+    </div>
+    
+   </div>
+   
+  </body>
+</html>
